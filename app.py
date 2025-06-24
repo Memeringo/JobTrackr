966cdb84a5e0f0ced7b6dde9ad758e810b7f0c8a
Memeringo <memeringo03@gmail.com>
2025-06-23 21:11:07 -0600

    Set up environment file support with dotenv

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from doctest import debug
-
+import os
 from flask import jsonify, Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client.get_database()
 
 @app.route("/")
 def home():
     return jsonify({"message":"JobTrackr is live!"})
+
+@app.route("/test-mongo")
+def test_mongo():
+    collections = db.list_collection_names()
+    return jsonify({"collections": collections})
 
 if __name__ == "__main__":
     app.run(debug = True)
